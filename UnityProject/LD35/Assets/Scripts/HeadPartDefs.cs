3dcc8c548c3c03874e1d6ee648a972966af6b77e
augusthippo <david.law.hein@gmail.com>
2016-04-16 19:04:56 -0600

    adding HeadPartDefs.cs

---
+++ UnityProject/LD35/Assets/Scripts/HeadPartDefs.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class HeadPartDefs
+{
+    public int m_ID
+    {
+        get; set;
+    }
+
+    public string name
+    {
+        get; set;
+    }
+
+    public string description
+    {
+        get; set;
+    }
+
+    public string assetName
+    {
+        get; set;
+    }
+
+    public int health
+    {
+        get; set;
+    }
+
+    public int damage
+    {
+        get; set;
+    }
+
+    public int turnSpeed
+    {
+        get; set;
+    }
+
+    public int jumpForce
+    {
+        get; set;
+    }
+}
