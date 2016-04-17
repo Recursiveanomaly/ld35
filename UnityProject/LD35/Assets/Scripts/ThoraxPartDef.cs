1c4710e35b891f65feeff7cad278858fe0cc277b
augusthippo <david.law.hein@gmail.com>
2016-04-16 19:17:14 -0600

    adding classes: AbdomenPartDef.cs, LegPartDef.cs, ThoraxPartDef.cs

---
+++ UnityProject/LD35/Assets/Scripts/ThoraxPartDef.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ThoraxPartDefs
+{
+    public int m_ID
+    {
+        get; set;
+    }
+
+    public string m_name
+    {
+        get; set;
+    }
+
+    public string m_description
+    {
+        get; set;
+    }
+
+    public string m_assetName
+    {
+        get; set;
+    }
+
+    public int m_health
+    {
+        get; set;
+    }
+
+    public int m_damage
+    {
+        get; set;
+    }
+
+    public int m_turnSpeed
+    {
+        get; set;
+    }
+
+    public int m_jumpForce
+    {
+        get; set;
+    }
+}
