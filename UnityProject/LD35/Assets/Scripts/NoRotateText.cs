a9afe14120a744e24f2c6cc9f55735b34ae25b78
augusthippo <david.law.hein@gmail.com>
2016-04-16 21:43:55 -0600

    adding NoRotateText.cs so that text on BeetleBotPrefab.prefab and BeetlePrefab.prefab wont rotate with the beetle

---
+++ UnityProject/LD35/Assets/Scripts/NoRotateText.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoRotateText : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.identity;
+    }    
+}
