ab9c8f55be8a95570f3969d579980662e5cf55c4
augusthippo <david.law.hein@gmail.com>
2016-04-17 22:07:18 -0600

    put bot spawning in loop for easier change of amounts, changed number to 20, changed width and height to 50

--- UnityProject/LD35/Assets/Scripts/RoomSetup.cs
+++ UnityProject/LD35/Assets/Scripts/RoomSetup.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class RoomSetup : MonoBehaviour 
 {
-    float m_roomWidth = 30;
-    float m_roomHeight = 30;
+    float m_roomWidth = 50;
+    float m_roomHeight = 50;
+    int m_numBots = 20;
 
     void OnJoinedRoom()
     {
@@ -13,11 +14,9 @@ public class RoomSetup : MonoBehaviour
             return;
         }
 
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
-        PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
+        for(int i = 0; i < m_numBots; i++)
+        {
+            PhotonNetwork.InstantiateSceneObject("BeetleBotPrefab", new Vector3(Random.Range(-m_roomWidth / 2, m_roomWidth / 2), Random.Range(-m_roomHeight / 2, m_roomHeight / 2), 0), Quaternion.identity, 0, null);
+        }
     }
 }
